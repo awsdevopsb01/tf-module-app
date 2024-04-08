b3dfe51d400400acbfde6b4321c91c8314d944f0
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-08 06:11:22 +0200

    creating new app servers

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
   ami                     = data.aws_ami.ami.id
   instance_type           = var.instance_type
-  subnet_ids              = var.subnet_id
+  subnet_ids              = var.subnet_ids
 }
