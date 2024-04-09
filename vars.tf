28a2cbaa540887ea21fe9b8f502c29fc82a66697
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-09 06:23:41 +0200

    tf-module-app - Auto scaling groups

--- vars.tf
+++ vars.tf
@@ -1,2 +1,9 @@
 variable "instance_type" {}
 variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "app_cidr_block" {}
+variable "vpc_id" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
